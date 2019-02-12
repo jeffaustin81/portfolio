7b6c89882a8b1a70a951ffb73c58c2213f5d377f
Jeff Austin <jeffaustin81@gmail.com>
2019-02-11 23:46:25 -0800

    inital commit, update README

---
+++ src/pages/index.js
@@ -0,0 +1,33 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+      <div style={{ width: `300px`, height: `300px` }}>
+        <Image />
+      </div>
+      <ul className="social-icons">
+        <li>
+          <a href="https://www.linkedin.com/in/jefferyaustin" target="_blank">
+            <span className="fab fa-linkedin"></span>
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/jeffaustin81" target="_blank">
+            <span className="fab fa-github-square"></span>
+          </a>
+        </li>
+        <li>
+          <a href="https://twitter.com/jeffaustin81" target="_blank">
+            <span className="fab fa-twitter-square"></span>
+          </a>
+        </li>
+      </ul>
+  </Layout>
+)
+
+export default IndexPage

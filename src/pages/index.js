384ab9aee547c68018d387bfb82375273df507db
Jeff Austin <jeffaustin@Jeffs-MacBook-Pro.local>
2019-06-08 01:18:12 -0700

    replace icon image with cdn link

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,13 @@
 import React from "react"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+// import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-      <div style={{ width: `300px`, height: `auto`, marginBottom: `30px` }}>
-        <Image />
-      </div>
+      <img width="300" src="https://res.cloudinary.com/dra8adhvz/image/upload/c_scale,w_500/v1559981488/icon-white-transparent_kr90l8.png" alt="JA icon"></img>
       <ul className="social-icons">
         <li>
           <a href="https://github.com/jeffaustin81" target="_blank" rel="noopener noreferrer">
